6e1d8c81c8c95b0690d8d2b2f80a19fe65bedc3a
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-01-02 00:37:23 +0900

    tailwind適用、style修正、レスポンシブ

--- order/src/App.vue
+++ order/src/App.vue
@@ -45,18 +45,18 @@ const form = {
           product_name: '箱推し12色ペンライト',
           fee_including_tax: '3500',
           quantity: '1',
-          img_path: 'https://placehold.jp/100x100.png',
+          img_path: 'https://placehold.jp/300x300.png',
           detail: [
-            { item_name: '乃木坂46', select_box: ['衛藤美彩', '橋本奈々未'], },
-            { item_name: '櫻坂46', select_box: '', },
-            { item_name: '日向坂46', select_box: '', },
+            { item_name: '乃木坂46', select_box: ['衛藤美彩', '橋本奈々未', '白石麻衣', '生田絵梨花', '与田祐希', '五百城茉央', '池田瑛紗'], },
+            { item_name: '櫻坂46', select_box: ['渡辺梨加', '守屋麗奈', '森田ひかる', '土生瑞穂', '田村保乃', '山﨑天'], },
+            { item_name: '日向坂46', select_box: ['小坂菜緒', '齊藤京子', '正源寺陽子',], },
           ],
         },
         {
           product_name: '推しメン別12色ペンライト',
           fee_including_tax: '4000',
           quantity: '3',
-          img_path: 'https://placehold.jp/100x100.png',
+          img_path: 'https://placehold.jp/300x300.png',
           detail: [
             { item_name: '乃木坂46', select_box: ['衛藤美彩', '橋本奈々未', '白石麻衣', '生田絵梨花', '与田祐希', '五百城茉央', '池田瑛紗'], },
             { item_name: '櫻坂46', select_box: ['渡辺梨加', '守屋麗奈', '森田ひかる', '土生瑞穂', '田村保乃', '山﨑天'] },
@@ -88,12 +88,9 @@ const form = {
           product_name: 'マフラータオル',
           fee_including_tax: '1500',
           quantity: '2',
-          img_path: 'https://placehold.jp/100x100.png',
+          img_path: 'https://placehold.jp/300x300.png',
           detail: [
-            { item_name: 'メンバーの名前', select_box: '', },
-            { item_name: '', select_box: '', },
-            { item_name: '', select_box: '', },
-            { item_name: '', select_box: '', },
+            { item_name: 'メンバーの名前', select_box: ['衛藤美彩', '橋本奈々未', '白石麻衣', '生田絵梨花', '与田祐希', '五百城茉央', '池田瑛紗', '渡辺梨加', '守屋麗奈', '森田ひかる', '土生瑞穂', '田村保乃', '山﨑天'], },
           ],
         },
       ],
@@ -110,44 +107,66 @@ const currentDate = dayjs();
 </script>
 
 <template>
-  <main>
-    <h1>order list</h1>
+
+  <header class="fixed pl-3 md:pl-6 pt-2 md:pt-4 text-xl bg-white w-full">
+    <h1>Sakamichi shop</h1>
+  </header>
+
+  <main class="py-24 px-2">
+
     <section>
-      <p>{{ currentDate.format('YYYY/MM/DD') }}</p>
-      <p>更新日：{{ dayjs(form.day).format('YYYY/MM/DD') }}</p>
-      <div class="content">
+
+      <h2 class="text-3xl mb-8 text-center">order list</h2>
+
+      <div class="w-48 mx-auto mb-6">
+        <p class="text-xs text-left mb-1">現在日時：{{ currentDate.format('YYYY/MM/DD hh:ss') }}</p>
+        <p class="text-xs text-left">更新日：{{ dayjs(form.day).format('YYYY/MM/DD') }}</p>
+      </div>
+      <p class="text-sm text-left md:text-center mb-4">購入商品とお客様情報をご確認の上、お支払い情報にお進みください。</p>
+
+      <div class="w-[20rem] md:w-[36rem] mx-auto shadow-xl rounded-3xl py-16 px-4 md:px-8">
         <ul>
-          <li v-for="itemBox, itemBoxIndex in form.item_box" :key="itemBoxIndex">
-            <p>購入商品{{ itemBox.number }}</p>
+          <li v-for="itemBox, itemBoxIndex in form.item_box" :key="itemBoxIndex" class="mb-12">
+            <h2 class="text-lg font-bold text-left mb-2">購入商品{{ itemBox.number }}</h2>
             <ul>
-              <li v-for="item, index in itemBox.items" :key="index">
+              <li v-for="item, index in itemBox.items" :key="index" class="" v-bind:class="{ 'mb-8': index !== itemBox.items.length - 1 }">
                 <div class="flex">
                   
-                  <div class="flex">
-                    <img :src="item.img_path" :alt="`${item.product_name}の画像`">
+                  <div class="flex items-start gap-x-4 flex-wrap md:flex-nowrap">
+                    <img :src="item.img_path" :alt="`${item.product_name}の画像`" class="w-48 object-cover rounded-lg mb-3 mx-auto md:mb-0">
                     <div>
-                      <p>{{ item.product_name }}</p>
-                      <p>{{ Number(item.fee_including_tax).toLocaleString() }}円<span>（税込）</span></p>
+                      <p class="text-left text-base mb-1">{{ item.product_name }}</p>
+                      <p class="text-left text-sm mb-4">{{ Number(item.fee_including_tax).toLocaleString() }}円<span class="text-xs">（税込）</span></p>
+                      <ul class="text-left">
+                        <li v-for="selectItem, selectItemIndex in item.detail" :key="selectItemIndex" class="" v-bind:class="{ 'mb-2': selectItemIndex !== item.detail.length -1 }">
+                          <label class="text-sm w-28 inline-block">{{ selectItem.item_name }}</label>
+                          <select v-model="selectItem.select_box" class="border border-gray-300 h-6 w-40 rounded-md">
+                            <option v-for="option in selectItem.select_box" :key="option" :value="option">{{ option }}</option>
+                          </select>
+                        </li>
+                      </ul>
                     </div>
                   </div>
-                  <ul>
-                    <li v-for="selectItem, selectItemIndex in item.detail" :key="selectItemIndex">
-                      <label>{{ selectItem.item_name }}</label>
-                      <select v-model="selectItem.select_box">
-                        <option v-for="option in selectItem.select_box" :key="option" :value="option">{{ option }}</option>
-                      </select>
-                    </li>
-                  </ul>
                 </div>
 
               </li>
             </ul>
           </li>
         </ul>
+
+        <hr />
+        <h2 class="text-lg font-bold text-left mb-2 mt-4">購入者情報</h2>
+
+        <hr />
+        <h2 class="text-lg font-bold text-left mb-2 mt-4">お届け先</h2>
+
       </div>
     </section>
   </main>
 
+  <div class="">
+    
+  </div>
 </template>
 
 <style>
@@ -155,28 +174,8 @@ const currentDate = dayjs();
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 ul,li {
   list-style: none;
-}
-.flex {
-  display: flex;
-}
-.content {
-  background-color: #fafafa;
-  border-radius: 20px;
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 50px 30px;
-}
-
-select {
-  width: 140px;
-  height: 30px;
-  padding: 1px 4px;
-  cursor: pointer;
 }
 </style>
