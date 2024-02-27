ffb4ce13df02b7fd19422a948520fe8419d9306c
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-02-27 22:12:54 +0900

    tailwind configにdropshadow追加

--- order/tailwind.config.js
+++ order/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        'shadow01': '3px 3px 8px rgba(0, 0, 0, 0.3)'
+      }
+    },
   },
   variants: {
     extend: {},
